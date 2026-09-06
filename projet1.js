1fa70f7df0022ef9a1f464b42e3efbb974f0bc5d
mohamed <simobriti12@gmail.com>
2026-09-06 04:42:17 +0100

    all done in projet 1

--- projet1.js
+++ projet1.js
@@ -4,18 +4,15 @@ let stock = [
     {titre:"salam",auteur:"Robert Kiyosak",prix:"25$",quantite:13},
     {titre:"hello",auteur:"Robert",prix:"25$",quantite:16},
 ]
-let len = 0
-    for(let i = 0;stock[i] != undefined;i++)
-        len++
 function    Afficher(){
-    for(let j = 0; j < len;j++){
+    for(let j = 0; j < stock.length;j++){
         console.log(`titre : ${stock[j].titre}; auteur : ${stock[j].auteur}; prix : ${stock[j].prix}; quantite : ${stock[j].quantite}`)
     }
 }
 function    Recherche(){
     let found = false
     let entre_titre = prompt("Entrer le titre de livre : ")
-    for(let i=0; i< len;i++){
+    for(let i=0; i< stock.length;i++){
         if(entre_titre == stock[i].titre){
             console.log(`titre : ${stock[i].titre}; auteur : ${stock[i].auteur}; prix : ${stock[i].prix}; quantite : ${stock[i].quantite}`)
             found = true
@@ -26,13 +23,13 @@ function    Recherche(){
         console.log("nous n'avons pas ce livre en stock ")
 }
 function    Ajouter(){
-    found = false
+    let found = false
     let n = prompt("Combien de livres souhaitez-vous ajouter ? : ")
     for(let i = 1;i<=n;i++){
         let add_titre = prompt(`Entrer le titre de livre ${i} : `)        
         let add_auteur = prompt(`Entrer le auteur de livre ${i} : `)        
         let add_prix = prompt(`Entrer le prix de livre ${i} : `)        
-        let add_quantite = prompt(`Entrer le quentite de livre ${i}: `)
+        let add_quantite = Number(prompt(`Entrer le quentite de livre ${i}: `))
         let new_livre = {
             titre:add_titre,
             auteur:add_auteur,
@@ -52,10 +49,10 @@ function    Ajouter(){
 function    Mettre_a_jour(){
     let found = false
     let n = prompt("Entrer le titre de livre pour Mettre a jour : ")
-    for(let i = 0;i<len;i++){
+    for(let i = 0;i<stock.length;i++){
         if(n == stock[i].titre){
             let new_titre = prompt("Entrer nouvelle titre : ")
-            let new_quentite = prompt("Entrer nouvelle quentite : ")
+            let new_quentite = Number(prompt("Entrer nouvelle quentite : "))
             stock[i].titre = new_titre
             stock[i].quantite = new_quentite
             found = true
@@ -74,24 +71,18 @@ function    Mettre_a_jour(){
         console.log("nous n'avons pas ce livre en stock ")
 }
 function    Supprimer(){
-    found = false
+    let found = false
     let nbdlo = []
     let x = prompt("entrez le nom du livre que vous souhaitez supprimer : ")
-    for(let i = 0;i < len;i++){
+    for(let i = 0;i < stock.length;i++){
         if (x == stock[i].titre) {
+            found = true
             continue
         }else{
-            let new_list = {
-            titre:stock[i].titre,
-            auteur:stock[i].auteur,
-            prix:stock[i].prix,
-            quantite:stock[i].quantite
+            nbdlo[nbdlo.length] = stock[i];
         }
-        
-        nbdlo[nbdlo.length] = stock[i];
-        }
+    }
     stock = nbdlo
-        }
     if(found == false)
         console.log("nous n'avons pas ce livre en stock")
     if(found == true){
@@ -105,10 +96,15 @@ function    Supprimer(){
     }
 }
 function    Nombre_stock(){
-    console.log(`le nombre total de livres en stock = ${len}`)
+    let res = 0
+    for(let i = 0; i < stock.length;i++)
+        res += stock[i].quantite
+    console.log(`le nombre total de livres en stock = ${res}`)
 }
-console.log("==== MENU ====\n 1. Ajouter un livre au stock\n 2. Afficher tous les livres disponibles\n 3. Rechercher un livre par son titre\n 4. Mettre à jour la quantité d'un livre\n 5. Supprimer un livre du stock\n 6. Afficher le nombre total de livres en stock.")
-let id = Number(prompt("choisir un option :"))
+let id = 0
+while(id != 7){
+console.log("==== MENU ====\n 1. Ajouter un livre au stock\n 2. Afficher tous les livres disponibles\n 3. Rechercher un livre par son titre\n 4. Mettre à jour la quantité d'un livre\n 5. Supprimer un livre du stock\n 6. Afficher le nombre total de livres en stock\n 7. Quitter")
+id = Number(prompt("choisir un option :"))
 switch(id){
     case 1:
         Ajouter()
@@ -128,6 +124,6 @@ switch(id){
     case 6:
         Nombre_stock()
         break;
-    default:
-        console.log("à venir")
-}
+    case 7:
+        break
+}}
